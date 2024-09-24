7b14bb2c329e2b58ab6b8d49781f5166686e194c
Kmk <56061215+MgKyawLay@users.noreply.github.com>
2024-09-24 17:12:31 +0630

    wire frame in item detail page

--- lib/view/item_detail.dart
+++ lib/view/item_detail.dart
@@ -1,13 +1,85 @@
+import 'package:coffee/assets/colors.dart';
+import 'package:coffee/view/components/category_tab.dart';
 import 'package:flutter/material.dart';
+import 'package:readmore/readmore.dart';
 
 class ItemDetail extends StatelessWidget {
+  const ItemDetail({super.key});
+
   @override
   Widget build(BuildContext context) {
+    final deviceHeight = MediaQuery.of(context).size.height;
     return Scaffold(
-      body: Center(
-        child: Text("data"),
-      ),
+        appBar: AppBar(
+          title: const Text("Detail"),
+          // scrolledUnderElevation: 0,
+          centerTitle: true,
+          leading: IconButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              icon: const Icon(Icons.chevron_left)),
+          actions: [
+            IconButton(
+                onPressed: () {
+                  Navigator.pushNamed(context, '/main');
+                },
+                icon: const Icon(Icons.favorite_border))
+          ],
+        ),
+        // bottomSheet: ,
+        body: SingleChildScrollView(
+            child: Stack(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Container(
+                        height: deviceHeight * 0.25,
+                        width: double.infinity,
+                        decoration: BoxDecoration(
+                          color: AppColors.overlayBackground,
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                      ),
+                      const Text("data"),
+                      const Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text("data"),
+                          Row(
+                            children: [
+                              Icon(
+                                Icons.star,
+                                color: Colors.yellow,
+                              ),
+                              Text("4.8")
+                            ],
+                          )
+                        ],
+                      ),
+                      const SizedBox(
+                        height: 30,
+                      ),
+                      const Text("data"),
+                      const ReadMoreText(
+                        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+                        trimLines: 3,
+                        trimMode: TrimMode.Line,
+                      ),
+                      const SizedBox(
+                        height: 30,
+                      ),
+                      Text("data"),
+                      CategoryTab(),
+                    ],
+                  ),
+                ),
+              ],
+            )
+        )
     );
   }
-  
 }
